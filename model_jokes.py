28dec853af31c8ff21676303561f74d7c7c906b3
CubNavarro <caleb.evan.navarro@gmail.com>
2022-10-25 11:13:55 -0700

    Api.pyrjhgoih

--- model_jokes.py
+++ model_jokes.py
@@ -2,26 +2,31 @@ import random
 
 jokes_data = []
 joke_list = [
-    "If you give someone a program... you will frustrate them for a day; if you teach them how to program... you will "
-    "frustrate them for a lifetime.",
-    "Q: Why did I divide sin by tan? A: Just cos.",
-    "UNIX is basically a simple operating system... but you have to be a genius to understand the simplicity.",
-    "Enter any 11-digit prime number to continue.",
-    "If at first you don't succeed; call it version 1.0.",
-    "Java programmers are some of the most materialistic people I know, very object-oriented",
-    "The oldest computer can be traced back to Adam and Eve. It was an apple but with extremely limited memory. Just "
-    "1 byte. And then everything crashed.",
-    "Q: Why did Wi-Fi and the computer get married? A: Because they had a connection",
-    "Bill Gates teaches a kindergarten class to count to ten. 1, 2, 3, 3.1, 95, 98, ME, 2000, XP, Vista, 7, 8, 10.",
-    "Q: What’s a aliens favorite computer key? A: the space bar!",
-    "There are 10 types of people in the world: those who understand binary, and those who don’t.",
-    "If it wasn't for C, we’d all be programming in BASI and OBOL.",
-    "Computers make very fast, very accurate mistakes.",
-    "Q: Why is it that programmers always confuse Halloween with Christmas? A: Because 31 OCT = 25 DEC.",
-    "Q: How many programmers does it take to change a light bulb? A: None. It’s a hardware problem.",
-    "The programmer got stuck in the shower because the instructions on the shampoo bottle said: Lather, Rinse, Repeat.",
-    "Q: What is the biggest lie in the entire universe? A: I have read and agree to the Terms and Conditions.",
-    'An SQL statement walks into a bar and sees two tables. It approaches, and asks may I join you?'
+    "What kind of exercise do lazy people do? Diddly-squats.",
+    "What do you call a pony with a cough? A little horse!",
+    "What is Forrest Gump's password? 1Forrest1.",
+    "Why did the M&M go to school? He wanted to be a Smartie.",
+    "What did one traffic light say to the other? Stop looking at me, I'm changing!",
+    "What do you call bears with no ears? B.",
+    "What's a foot long and slippery? A slipper!",
+    "Why do French people eat snails? They don't like fast food!",
+    "What's red and moves up and down? A tomato in an elevator!",
+    "I invented a new word today: Plagiarism.",
+    "What is sticky and brown? A stick!",
+    "How does a rabbi make coffee? Hebrews it!",
+    "Rest in peace boiling water. You will be mist!",
+    "How do you throw a space party? You planet!",
+    "Want to hear a construction joke? Oh never mind, I'm still working on that one.",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "I hate Russian dolls… they're so full of themselves!",
+    "Talk is cheap? Have you ever talked to a lawyer?",
+    "Why did the gym close down? It just didn't work out!",
+    "Two artists had an art contest. It ended in a draw!",
+    "A plateau is the highest form of flattery.",
+    "I have a fear of speed bumps. But I am slowly getting over it.",
+    "You can only get spoiled milk from a pampered cow.",
+    "What do you call a boomerang that doesn't come back? A stick!",
+    "You know what I saw today? Everything I looked at."
 ]
 
 # Initialize jokes
