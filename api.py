b8040a12dacdfb0e5434d308ac4c33e7200dfda8
CubNavarro <caleb.evan.navarro@gmail.com>
2022-10-28 11:46:56 -0700

    kkkkkkkkkkklkoiguhbgvyugvk

--- api.py
+++ api.py
@@ -62,8 +62,8 @@ class JokesAPI:
     
 if __name__ == "__main__": 
     # server = "http://127.0.0.1:5000" # run local
-    server = 'http://tngc.nighthawkcodescrums.gq/' # run from web
-    url = server + "/api/jokes"
+    server = 'http://tngc.nighthawkcodescrums.gq' # run from web
+    url = server + "api/jokes"
     responses = []  # responses list
 
     # get count of jokes on server
