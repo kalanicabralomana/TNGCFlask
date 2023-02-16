bbb60152d56a6017d4e61146cd7898a0e5db5484
kcabralomana <kcabralomana@gmail.com>
2023-02-16 14:25:47 -0800

    afds

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
 # import "packages" from "this" project
-from __init__ import app  # Definitions initialization
+from __init__ import app , db # Definitions initialization
 from chess import app_api2 # Blueprint import api definition
-from users import user_api
 from superCoolFile import chess_user_api
 from server1 import server1
 from testApi import server2
 from bp_projects.projects import app_projects # Blueprint directory import projects definition
+from apis.roulette import roulette_bp
+from model.roulette import init_roulettes
 
 # app.register_blueprint(app_api2) # register api routes
 # app.register_blueprint(user_api)
@@ -15,6 +16,7 @@ app.register_blueprint(chess_user_api)
 app.register_blueprint(server1)
 app.register_blueprint(server2)
 app.register_blueprint(app_projects) # register api routes
+app.register_blueprint(roulette_bp)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -37,6 +39,16 @@ def team():
 def chess():
     return render_template("chess.html")
 
+@app.before_first_request
+def init_db():
+    with app.app_context():
+        db.create_all()
+        init_roulettes()
+
 # this runs the application on the development server
 if __name__ == "__main__":
+    from flask_cors import CORS
+
+    cors = CORS(app)
+    db.init_app(app)
     app.run(debug=True)
